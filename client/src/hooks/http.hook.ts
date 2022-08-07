6a52c34ce69dbab86881feef9992b6174568b8bf
Alex-Evo_2266 <arkhipovAR2002@mail.ru>
2022-08-07 14:53:10 +0300

    413

--- client/src/hooks/http.hook.ts
+++ client/src/hooks/http.hook.ts
@@ -23,44 +23,46 @@ export const useHttp = () => {
 	const [error, setError] = useState<string | null>(null);
 
   const refresh = useCallback(async ()=>{
-    const response = await fetch("/api/auth/refresh", {method:"GET", body:null, headers:{}});
-    const data = await response.json()
-    if (!response.ok) {
+	const response = await fetch("/api/auth/refresh", {method:"GET", body:null, headers:{}});
+	const data = await response.json()
+	if (!response.ok) {
 	  dispatch({type: "LOGOUT"})
-      throw new Error(data.message||'что-то пошло не так')
-    }
+	  throw new Error(data.message||'что-то пошло не так')
+	}
 	dispatch({type: "REFRASH", payload:{token: data.token, expires_at: data.expires_at}})
-    return data.token
+	return data.token
   },[dispatch])
 
   const request = useCallback(async (url:string, method:methods=methods.GET , body = null, headers = {},file:boolean=false) => {
-    setLoading(true);
-    try {
-      if(headers['Authorization'])
-      {
-        headers['Authorization-Token'] = headers['Authorization']
-        // headers['Authorization'] = undefined
-      }
-      if(body&&!file){
-        headers['Content-Type'] = 'application/json'
-        body = JSON.stringify(body);
-      }
-      let response = await fetch(url, {method, body, headers});
-      if (response.status === 401){
-        const token = await refresh()
-        headers["Authorization-Token"] = `Bearer ${token}`
-        response = await fetch(url, {method, body, headers});
-      }
-      const data = await response.json()
-      if (!response.ok) {
-        throw new Error(data.message||'что-то пошло не так')
-      }
-      setLoading(false);
-      return data;
-    } catch (e) {
+	setLoading(true);
+	try {
+	  if(headers['Authorization'])
+	  {
+		headers['Authorization-Token'] = headers['Authorization']
+		// headers['Authorization'] = undefined
+	  }
+	  if(body&&!file){
+		headers['Content-Type'] = 'application/json'
+		body = JSON.stringify(body);
+	  }
+	  let response = await fetch(url, {method, body, headers});
+	  if (response.status === 401){
+		const token = await refresh()
+		headers["Authorization-Token"] = `Bearer ${token}`
+		response = await fetch(url, {method, body, headers});
+	  }
+	  const data = await response.json()
+	  if (!response.ok) {
+		if (response.status === 413)
+			throw new Error('Request Entity Too Large' + data.message)
+		throw new Error(data.message||'что-то пошло не так')
+	  }
+	  setLoading(false);
+	  return data;
+	} catch (e) {
 		setLoading(false)
 		setError(getErrorMessage(e))
-    }
+	}
   },[refresh]);
 
   const clearError = useCallback(() => {setError(null)},[]);
